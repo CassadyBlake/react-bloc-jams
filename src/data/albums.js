9d288068cd052b65b17230301889ee1ea5a3b289
CassadyBlake <cassady.lillstrom@gmail.com>
2018-02-05 15:45:09 -0800

    blocJams react audio: completed

--- src/data/albums.js
+++ src/data/albums.js
@@ -1,4 +1,17 @@
 export default [{
+    title: 'Lovejoys',
+    artist: 'Pickwick',
+    releaseInfo: 'Small Press Records',
+    albumCover: '/assets/images/album_covers/22.jpg',
+    slug: 'lovejoys',
+    songs: [
+      { title: 'Thought It Was You', duration: '246.86', audioSrc: '/assets/music/Thought.mp3' },
+      { title: 'Turncoat', duration: '198.6', audioSrc: '/assets/music/Turncoat.mp3' },
+      { title: 'In Time', duration: '175.8', audioSrc: '/assets/music/InTime.mp3' },
+      { title: 'Ascension', duration: '245.4', audioSrc: '/assets/music/Ascension.mp3' },
+      { title: 'Lying Awake In The Dark', duration: '277.8', audioSrc: '/assets/music/Lying.mp3' }
+    ]
+}, {
   title: 'The Colors',
   artist: 'Pablo Picasso',
   releaseInfo: '1909 Spanish Records',
@@ -23,18 +36,5 @@ export default [{
       { title: 'Red', duration: '268.45', audioSrc: '/assets/music/red.mp3' },
       { title: 'Pink', duration: '153.14', audioSrc: '/assets/music/pink.mp3' },
       { title: 'Magenta', duration: '374.22', audioSrc: '/assets/music/magenta.mp3' }
-    ]
-}, {
-    title: 'Lovejoys',
-    artist: 'Pickwick',
-    releaseInfo: 'Small Press Records',
-    albumCover: '/assets/images/album_covers/22.jpg',
-    slug: 'lovejoys',
-    songs: [
-      { title: 'Thought It Was You', duration: '246.86', soundFileUrl: 'assets/music/Thought.mp3' },
-      { title: 'Turncoat', duration: '198.6', soundFileUrl: 'assets/music/Turncoat.mp3' },
-      { title: 'In Time', duration: '175.8', soundFileUrl: 'assets/music/InTime.mp3' },
-      { title: 'Ascension', duration: '245.4', soundFileUrl: 'assets/music/Ascension.mp3' },
-      { title: 'Lying Awake In The Dark', duration: '277.8', soundFileUrl: 'assets/music/Lying.mp3' }
     ]
 }];
